cc85dd8cf598b5d4d706422255f65cee894f1a8c
EMilborn <emilborn@stuy.edu>
2017-03-02 14:51:09 -0500

    Create javacsript.js

---
+++ javacsript.js
@@ -0,0 +1,21 @@
+var rid = 0;
+
+var grow = functon(){
+  window.cancelAnimationFrame(rid);
+  
+  var r = 0;
+  var grow = 0;
+  
+  var draw = function(){
+    if (s.firstChild){
+      s.removeChild(s.firstChild);
+      };
+    circ = document.creatElementNS9(http://www.w3.org/2000/svg", "circle");
+    circ.setAttribute("r", r);
+    circ.setAttribute("cx", 249.5);
+    circ.setAttribute("cy", 249.5);
+    
+    };
+};
+    
+    
